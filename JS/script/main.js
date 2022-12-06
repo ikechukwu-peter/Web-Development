fa280e1ac83ec7fdac3541158de9ab7d4b252380
ikechukwu-peter <pitiyke@gmail.com>
2022-12-06 19:08:33 +0100

    feat: js

---
+++ JS/script/main.js
@@ -0,0 +1,50 @@
+// document.write("MR GOAT");
+
+//keyword for naming variables in JS
+// var, let, const
+
+//JS SCOPES
+
+//local scope
+
+//global scope
+
+//TYPES OF DATA
+//string
+//number
+//undefined
+//null
+
+// var firstName = "John";
+// var age = 20;
+// let numberOfOranges;
+// const data = null;
+
+//ARRAY
+
+// let fruits = ["orange", "pineapple", "mango", "strawberry"];
+
+// console.log(fruits.length);
+
+// console.log(fruits[0])
+// console.log(fruits.at(2));
+
+// let matric = [[1110], [1101], "mango", 1];
+
+// console.log(matric);
+
+// const person = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   age: 20,
+//   hobbies: ["music", "football"],
+//   location: {
+//     city: "Lagos",
+//     country: "Nigeria",
+//   },
+// };
+
+// console.log(person);
+
+// console.log(person["location"]["city"]);
+// console.log(person.location.country);
